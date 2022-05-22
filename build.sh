212cc133f6eb2bfdb083f0a8b1d5b638a5dcbe8a
叶胜 <shengye@kugou.net>
2022-05-22 23:28:23 +0800

    init

--- build.sh
+++ build.sh
@@ -2,6 +2,10 @@
 
 DOCKER_DIR=${JOB_NAME}
 
-echo "docker workspace : #{JOB_NAME}"
+echo "docker workspace : ${JOB_NAME}"
 
 JENKINS_DIR=${WORKSPACE}/${MODULE}
+
+
+echo "docker workspace : ${JENKINS_DIR}"
+
