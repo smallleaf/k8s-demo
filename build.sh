d4f0f1f622b82e6bdfcba8eae35031db9e270813
叶胜 <shengye@kugou.net>
2022-06-06 22:39:17 +0800

    init

--- build.sh
+++ build.sh
@@ -17,21 +17,5 @@ scp ${MODULE}/${MODULE}.yaml root@k8s1:/data/
 
 echo "push success"
 
-echo "login k8s"
-
-ssh -tt k8s1
-
-echo "login success"
-
-cat /data/${MODULE}.yaml
-
-echo "exec kubectl apply -f ${filePath}"
-
-kubectl apply -f ${filePath}
-
-echo "exec success"
-exit
-
-echo "exit deply success"
 
 
