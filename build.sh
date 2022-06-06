ddc5a3f0526f3fc1f9202c624cc66ff9a7286bc9
叶胜 <shengye@kugou.net>
2022-06-06 21:29:06 +0800

    init

--- build.sh
+++ build.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
 echo "workspace : ${WORKSPACE},module: ${MODULE}"
-rm -rf Dockerfile
-touch Dockerfile
 
-echo "FROM apache/skywalking-java-agent:8.10.0-java8" > Dockerfile
-echo "MAINTAINER yesheng 578986218@qq.com" > Dockerfile
-cat "COPY ${MODULE}/target/${MODULE}-1.0.jar /${MODULE}.jar" > Dockerfile
-cat "ENTRYPOINT java $JAVA_OPTS  -jar /${MODULE}.jar" > Dockerfile
-
-cat Dockerfile
-
-#docker build -t 578986218/${MODULE}:latest .
-#docker push 578986218/${MODULE}:latest .
+docker build -t 578986218/${MODULE}:latest .
+docker push 578986218/${MODULE}:latest .
 
