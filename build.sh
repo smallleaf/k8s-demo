791fe6e35000a9f7b5201227d3d2895a44a1d771
叶胜 <shengye@kugou.net>
2022-06-06 21:38:18 +0800

    init

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 echo "workspace : ${WORKSPACE},module: ${MODULE}"
 
-sed 's/module/${MODULE}/g' Dockerfile
+sed -i  's/module/${MODULE}/g' Dockerfile
 
 docker build -t 578986218/${MODULE}:latest .
 docker push 578986218/${MODULE}:latest .
