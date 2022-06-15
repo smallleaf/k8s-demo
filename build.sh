4ec85dfb96de9542245d54bd410437c227b405ad
叶胜 <shengye@kugou.net>
2022-06-15 19:37:23 +0800

    init

--- build.sh
+++ build.sh
@@ -16,6 +16,11 @@ echo "push file :${filePath} to k8s1"
 scp ${MODULE}/${MODULE}.yaml root@k8s1:/data/
 
 echo "push success"
+echo "start exec k8s apply : kubectl apply -f ${filePath}"
+
+ssh k8s1 "kubectl apply -f ${filePath}"
+echo "exec success"
+
 
 
 
